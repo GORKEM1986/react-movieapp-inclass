4f8e38f0ab5d09e2e50be95ad956b1ea3398dc1e
GORKEM1986 <aslanakbey7106@gmail.com>
2023-03-24 15:32:37 +0100

    first commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,128 @@
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import React, { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { auth } from "../auth/firebase";
+import {
+  toastErrorNotify,
+  toastSuccessNotify,
+  toastWarnNotify,
+} from "../helpers/ToastNotify";
+
+// export const {Provider} = createContext()
+export const AuthContext = createContext();
+//* with custom hook
+// export const useAuthContext = () => {
+//     return useContext(AuthContext);
+//   };
+
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("user")) || false
+  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    userObserver();
+  }, []);
+
+  const createUser = async (email, password, displayName) => {
+    try {
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+
+      await updateProfile(auth.currentUser, {
+        displayName: displayName,
+      });
+      navigate("/");
+      toastSuccessNotify("Registered Succesfully");
+    } catch (error) {
+      console.log(error);
+      toastErrorNotify(error.message);
+    }
+  };
+
+  const signIn = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+      toastSuccessNotify("Logged in Succesfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  const userObserver = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { email, displayName, photoURL } = user;
+        setCurrentUser({ email, displayName, photoURL });
+        sessionStorage.setItem(
+          "user",
+          JSON.stringify({ email, displayName, photoURL })
+        );
+      } else {
+        setCurrentUser(false);
+        sessionStorage.clear();
+        // console.log('logged out');
+      }
+    });
+  };
+
+  const signUpProvider = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        toastSuccessNotify("Logged in Succesfully!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const forgotPassword = (email) => {
+    //? Email yoluyla şifre sıfırlama için kullanılan firebase metodu
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        // Password reset email sent!
+        toastWarnNotify("Please check your mail box!");
+        // alert("Please check your mail box!");
+      })
+      .catch((err) => {
+        toastErrorNotify(err.message);
+        // alert(err.message);
+        // ..
+      });
+  };
+
+  const values = {
+    createUser,
+    signIn,
+    logOut,
+    signUpProvider,
+    forgotPassword,
+    currentUser,
+  };
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContextProvider;
